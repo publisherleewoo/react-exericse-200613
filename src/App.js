b7c248a2a41029e771313990082ee0c0662c9ef9
publisherleewoo <dlguddn3214@naver.com>
2020-06-21 23:40:42 +0900

    ValidationSample 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import EventPractice from './EventPractice';
-import EventPracticeF from './EventPracticeF';
+// import EventPractice from './EventPractice';
+// import EventPracticeF from './EventPracticeF';
 import ValidationSample from './ValidationSample';
 // import EventPracticeFList from './EventPracticeFList';
 
--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -1,37 +1,42 @@
 import React, { useState } from "react"
-import "./test.css"
+import "./ValidationSample.css"
+import { useCallback } from "react"
+import { useRef } from "react"
 const ValidationSample = () => {
-  const input = React.createRef()
+  const input =useRef()
   const [form, setForm] = useState({
     password: "",
-    clicked: false,
+    clicked: null,
     validated: false,
   })
   const { password, clicked, validated } = form
-  const handleChange = (e) => {
+  const handleChange = useCallback((e) => {
     const nextItem = {
       ...form,
       [e.target.name]: e.target.value,
     }
     setForm(nextItem)
-  }
-  const handleButtonClick = () => {
+  }, [form])
+  const handleButtonClick = useCallback(() => {
     const validated = password === "000"
     setForm({ ...form, clicked: true, validated })
     if (!validated) {
       input.current.focus()
-      alert("포커스")
     }
-  }
+  }, [form,input,password])
 
   return (
     <div>
+      <span className="success">비밀번호 000 입력시</span>
+      <br />
+      <span className="failure">그 외 </span>
+      <br />
       <input
         type="password"
         value={password}
         name="password"
         onChange={handleChange}
-        className={clicked ? (validated ? "success" : "failure") : ""}
+        className={clicked && (validated ? "success" : "failure") }
         ref={input}
       />
       <button onClick={handleButtonClick}> 검증하기</button>
